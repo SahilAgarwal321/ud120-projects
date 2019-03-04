03fdf450d322d9f91dee45493ab00c0a639ea91f
Sahil Agarwal <sahil.agarwal@thesmartcube.com>
2019-03-04 14:39:12 +0530

    Calculating Author ID Accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -26,8 +27,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+t1 = time()
+accuracy = clf.score(features_test, labels_test)
+# preds = clf.predict(features_test)
+t2 = time()
 
-
+print "Author ID Accuracy:", accuracy
+print "Time taken in training/fitting:", t1-t0, "seconds"
+print "Time taken in prediction:", t2-t1, "seconds"
 #########################################################
 
 
